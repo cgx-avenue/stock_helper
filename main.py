ea07ef272450546423cdf22455cf419fe9504156
cgx-avenue <zuomujie_2019@qq.com>
2024-03-12 13:53:08 +0800

    modify all records table with ui.refreshable

--- main.py
+++ main.py
@@ -26,18 +26,20 @@ def get_all_records():
 df_all_records = get_all_records()
 
 # use akshare interface to get all ETF real-time prices
-dfetf = ak.fund_etf_spot_em()
+df_etf = ak.fund_etf_spot_em()
 
 
 def get_overview_table():
     df = df_all_records.copy()
 
-    df_buy = df[df['action'] == 'BUY'].groupby(['code', 'stock_name']).agg(
-        {'amount': sum, 'quantity': sum})
-    df_sell = df[df['action'] == 'SELL'].groupby(['code', 'stock_name']).agg(
-        {'amount': sum, 'quantity': sum})
-    df_buy.reset_index(inplace=True)
-    df_sell.reset_index(inplace=True)
+    def group_table(df, action):
+        res = df[df['action'] == action].groupby(['code', 'stock_name']).agg(
+            {'amount': sum, 'quantity': sum})
+        res.reset_index(inplace=True)
+        return res
+
+    df_buy = group_table(df, 'BUY')
+    df_sell = group_table(df, 'SELL')
     df_agg = df_buy.join(df_sell.set_index(
         'code'), on='code', rsuffix='_sell').fillna(0)
     df_agg['amount'] = df_agg['amount']-df_agg['amount_sell']
@@ -49,18 +51,21 @@ def get_overview_table():
     df_agg['cost'] = df_agg['amount'].apply(
         lambda x: float(x))/df_agg['quantity']
 
-    df = df_agg.join(dfetf.set_index('代码'), on='code')
+    df = df_agg.join(df_etf.set_index('代码'), on='code')
     df = df.drop(['stock_name', '成交量', '成交额', '流通市值', '总市值'], axis=1)
     df[["最新价", "涨跌额"]] = df[["最新价", "涨跌额"]].apply(pd.to_numeric)
-    df['涨跌幅'].apply(lambda x: '%.2f%%' % x)
+
     df['现价值'] = df['最新价']*df['quantity']
-    df['差价'] = (df['最新价']-df['cost']).apply(lambda x: '%.3f' % x)
+    df['差价'] = (df['最新价']-df['cost'])
     df['差幅'] = (100*(df['最新价']-df['cost'])/df['cost']
-                ).apply(lambda x: '%.2f%%' % x)
+                )
 
-    df['cost'] = df['cost'].apply(lambda x: '%.3f' % x)
-    df['profit'] = (df['现价值']-df['amount']).apply(lambda x: '%.3f' % x)
-
+    df['cost'] = df['cost']
+    df['profit'] = (df['现价值']-df['amount'])
+    df[['差幅', '涨跌幅']] = df[['amount', '涨跌幅']].map(lambda x: '%.2f%%' % x)
+    df[['amount', '现价值']] = df[['amount', '现价值']].map(lambda x: '%.2f' % x)
+    df[['差价', 'cost', 'profit']] = df[[
+        '差价', 'cost', 'profit']].map(lambda x: '%.3f' % x)
     return df
 
 
@@ -71,7 +76,7 @@ codes = list(df_overview['code'].values)
 
 def bind_price_to_all_records():
     # process detailed transaction data
-    df = df_all_records.copy().join(dfetf.set_index('代码'), on='code')
+    df = df_all_records.copy().join(df_etf.set_index('代码'), on='code')
     df = df.drop(['stock_name', '成交量', '成交额', '流通市值', '总市值', '涨跌额',
                  '涨跌幅', '开盘价', '最高价', '最低价', '昨收', '换手率'], axis=1)
     df[["price", "quantity", 'amount', 'fee']] = df[[
@@ -86,6 +91,8 @@ def bind_price_to_all_records():
         df['action'] == 'SELL', df['profit']*(-1)).apply(lambda x: '%.3f' % x)
 
     return df
+
+df_all_records_price=bind_price_to_all_records()
 
 
 def close_app():
@@ -106,13 +113,10 @@ def input_new_transaction():
     conn.commit()
     df_all_records = get_all_records()
     ui_all_records.update_rows(df_all_records.to_dict(orient='records'))
-    ui_overview_table.update_rows(
-        bind_price_to_all_records().to_dict(orient='records'))
-    with ui.dialog() as dialog,ui.card():
+    update_overview_table_ui()
+    with ui.dialog() as dialog, ui.card():
         ui.label('Insertion done!').style('color: green; font-size: 200%')
-        ui.button('Close',on_click=dialog.close)
-
-
+        ui.button('Close', on_click=dialog.close)
 
 
 with ui.expansion('Add new transaction!', icon='add').classes('w-full').style(label_style):
@@ -124,7 +128,7 @@ with ui.expansion('Add new transaction!', icon='add').classes('w-full').style(la
         price = ui.number(label='Price', format='%.3f', step=0.1).props(
             'clearable').style('width:10%')
         quantity = ui.number(label='Quantity',  placeholder='step by 100', value=100, step=100,
-                            validation={'Input ': lambda value: value > 0}).style('width:6%').props('clearable')
+                             validation={'Input ': lambda value: value > 0}).style('width:6%').props('clearable')
         fee = ui.number(label='Fee', placeholder='trasaction cost', step=0.1,
                         validation={'Input too long': lambda value: value > 0}).style('width:6%').props('clearable')
         action = ui.radio(['BUY', 'SELL'], value='BUY').props('inline')
@@ -132,23 +136,31 @@ with ui.expansion('Add new transaction!', icon='add').classes('w-full').style(la
         ui.button('Add', icon='add', on_click=input_new_transaction)
 
 
-
-
-
 ui.separator()
 ui.label('Overview').style(label_style)
 
 
-
-
-ui_overview_table = ui.table.from_pandas(get_overview_table())
-ui_overview_table.add_slot('body-cell-profit', '''
+@ui.refreshable
+def overtiew_table_ui() -> None:
+    ui.table.from_pandas(df_overview).add_slot('body-cell-profit', '''
     <q-td key="profit" :props="props">
         <q-badge :color="props.value < 0 ? 'red' : 'green'">
             {{ props.value }}
         </q-badge>
     </q-td>
 ''')
+
+
+def update_overview_table_ui() -> None:
+    global df_etf, df_overview
+    df_etf = ak.fund_etf_spot_em()
+    df_overview = get_overview_table()
+    overtiew_table_ui.refresh()
+
+
+overtiew_table_ui()
+ui.button('update', on_click=update_overview_table_ui)
+
 ui.separator()
 
 
@@ -191,15 +203,27 @@ fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
 ui_plot = ui.plotly(fig).classes('w-full h-60')
 update_plot()
 
-ui_all_records = ui.table.from_pandas(
-    bind_price_to_all_records(), row_key='code').bind_filter_from(ui_code_selector, 'value')
-ui_all_records.add_slot('body-cell-profit', '''
+@ui.refreshable
+def all_records_table_ui() -> None:
+    ui.table.from_pandas(
+    bind_price_to_all_records(), row_key='code').bind_filter_from(ui_code_selector, 'value').add_slot('body-cell-profit', '''
     <q-td key="profit" :props="props">
         <q-badge :color="props.value < 0 ? 'red' : 'green'">
             {{ props.value }}
         </q-badge>
     </q-td>
 ''')
+    
+def update_all_records_table_ui() -> None:
+    global df_etf,df_all_records_price
+    df_etf = ak.fund_etf_spot_em()
+    df_all_records_price = bind_price_to_all_records()
+    all_records_table_ui.refresh()
+
+all_records_table_ui()
+ui.button('update', on_click=update_all_records_table_ui)
+
+
 
 ui.separator()
 
