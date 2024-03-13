b3922ff566b4ae25132ff7d03f67800705d7f665
cgx-avenue <zuomujie_2019@qq.com>
2024-03-13 16:20:44 +0800

    refactor table code

--- main.py
+++ main.py
@@ -9,31 +9,36 @@ import numpy as np
 conn = sqlite3.connect('stock_trading_records.db')
 cur = conn.cursor()
 
+ui.dark_mode().enable()
 ui.page_title("Stock_transaction_tool")
 
 label_style = 'color: #6E93D6; font-size: 200%; font-weight: 300'
 
+
 def get_all_records():
     all_records = pd.read_sql(
         "select code,stock_name,timestamp,action,price,quantity,amount,fee from stock_trading_record order by timestamp desc", con=conn)
-    print(all_records.shape)
+    # print(all_records.shape)
     return all_records
+
 
 df_all_records = get_all_records()
 
 df_etf = ak.fund_etf_spot_em()
 
+
+def group_table_by_action(df, action):
+    res = df[df['action'] == action].groupby(['code', 'stock_name']).agg(
+        {'amount': 'sum', 'quantity': 'sum'})
+    res.reset_index(inplace=True)
+    return res
+
+
 def get_overview_table():
     df = df_all_records.copy()
 
-    def group_table(df, action):
-        res = df[df['action'] == action].groupby(['code', 'stock_name']).agg(
-            {'amount': sum, 'quantity': sum})
-        res.reset_index(inplace=True)
-        return res
-
-    df_buy = group_table(df, 'BUY')
-    df_sell = group_table(df, 'SELL')
+    df_buy = group_table_by_action(df, 'BUY')
+    df_sell = group_table_by_action(df, 'SELL')
     df_agg = df_buy.join(df_sell.set_index(
         'code'), on='code', rsuffix='_sell').fillna(0)
     df_agg['amount'] = df_agg['amount']-df_agg['amount_sell']
@@ -43,36 +48,40 @@ def get_overview_table():
     df_agg = df_agg.drop(
         ['stock_name_sell', 'amount_sell', 'quantity_sell'], axis=1)
     df_agg['cost'] = df_agg['amount'].apply(
-        lambda x: float(x))/df_agg['quantity'] 
+        lambda x: float(x))/df_agg['quantity']
 
     df = df_agg.join(df_etf.set_index('代码'), on='code')
     df = df.drop(['stock_name', '成交量', '成交额', '流通市值', '总市值'], axis=1)
     df[["最新价", "涨跌额"]] = df[["最新价", "涨跌额"]].apply(pd.to_numeric)
 
     df['现价值'] = df['最新价']*df['quantity']
-    df['差价'] = (df['最新价']-df['cost']) 
+    df['差价'] = (df['最新价']-df['cost'])
     df['差幅'] = (100*(df['最新价']-df['cost'])/df['cost']
-                ) 
+                )
 
     df['cost'] = df['cost']
     df['profit'] = (df['现价值']-df['amount'])
     df.fillna(0)
-    df.replace([np.inf,-np.inf],0,inplace=True)
+    df.replace([np.inf, -np.inf], 0, inplace=True)
     df[['差幅', '涨跌幅']] = df[['差幅', '涨跌幅']].map(lambda x: '%.2f%%' % x)
     df[['amount', '现价值']] = df[['amount', '现价值']].map(lambda x: '%.2f' % x)
     df[['差价', 'cost', 'profit']] = df[[
         '差价', 'cost', 'profit']].map(lambda x: '%.3f' % x)
-    df=df.sort_values(by=['quantity','amount'],ascending=False)
+    df = df.sort_values(by=['quantity', 'amount'], ascending=False)
     return df
+
 
 df_overview = get_overview_table()
 
 all_stock_codes = list(df_overview['code'].values)
 
+
 def get_total_profit(df_overview) -> float:
     return round(df_overview['profit'].apply(pd.to_numeric).sum(), 2)
 
+
 total_profit = get_total_profit(df_overview)
+
 
 def bind_price_to_all_records():
     # process detailed transaction data
@@ -92,13 +101,17 @@ def bind_price_to_all_records():
 
     return df
 
+
 df_all_records_price = bind_price_to_all_records()
+
 
 def close_app():
     cur.close()
     conn.close()
 
+
 app.on_shutdown(close_app)
+
 
 def input_new_transaction():
     amount = price.value*quantity.value
@@ -110,8 +123,10 @@ def input_new_transaction():
     conn.commit()
     update_2_tables_ui()
     with ui.dialog() as dialog, ui.card():
-        ui.label('New transaction added!').style('color: green; font-size: 200%')
+        ui.label('New transaction added!').style(
+            'color: green; font-size: 200%')
         ui.button('Close', on_click=dialog.close)
+
 
 with ui.expansion('Add new transaction!', icon='add').classes('w-full').style(label_style):
     with ui.row():
@@ -137,6 +152,7 @@ with ui.row():
     ui_total_profit = ui.label().bind_text(globals(), 'total_profit').style(
         'color:green;font-size: 100%; font-weight: 300' if total_profit > 0 else 'color:red;font-size: 100%; font-weight: 300;margin-top:15px')
 
+
 @ui.refreshable
 def overtiew_table_ui() -> None:
     ui.table.from_pandas(df_overview, pagination=10).add_slot('body-cell-profit', '''
@@ -147,40 +163,30 @@ def overtiew_table_ui() -> None:
     </q-td>
 ''')
 
+
 overtiew_table_ui()
 
 ui.separator()
 
+def add_trace_to_fig(df_all_records, action, code):
+    df = df_all_records[df_all_records['action'] == action].copy()
+    ts = df[df['code'] == code]['timestamp'].to_list()
+    prices = df[df['code'] == code]['price'].to_list()
+    quantity = df[df['code'] == code]['quantity'].to_list()
+    size_buy = list(map(lambda x: x/100, quantity))
+    fig.add_trace(go.Scatter(x=ts, y=prices, text=quantity,
+                  marker_size=size_buy, hoverinfo='y + text', name=action))
+
+
 def update_plot() -> None:
     fig.data = []
-    df_buy = df_all_records[df_all_records['action'] == 'BUY'].copy()
-    df_sell = df_all_records[df_all_records['action'] == 'SELL'].copy()
-    ts_buy = df_buy[df_all_records['code'] ==
-                    ui_code_selector.value]['timestamp'].to_list()
-    prices_buy = df_buy[df_all_records['code']
-                        == ui_code_selector.value]['price'].to_list()
-    quantity_buy = df_buy[df_all_records['code']
-                          == ui_code_selector.value]['quantity'].to_list()
-    size_buy = list(map(lambda x: x/100, quantity_buy))
-    ts_sell = df_sell[df_all_records['code'] ==
-                      ui_code_selector.value]['timestamp'].to_list()
-    prices_sell = df_sell[df_all_records['code']
-                          == ui_code_selector.value]['price'].to_list()
-    quantity_sell = df_sell[df_all_records['code']
-                            == ui_code_selector.value]['quantity'].to_list()
-    size_sell = list(map(lambda x: x/100, quantity_sell))
-
-    fig.add_trace(go.Scatter(x=ts_buy, y=prices_buy, text=quantity_buy,
-                  marker_size=size_buy, hoverinfo='y + text', name='Buy'))
-    fig.add_trace(go.Scatter(x=ts_sell, y=prices_sell, text=quantity_sell,
-                  marker_size=size_sell, hoverinfo='y + text', name='Sell'))
-
+    add_trace_to_fig(df_all_records, 'BUY', ui_code_selector.value)
+    add_trace_to_fig(df_all_records, 'SELL', ui_code_selector.value)
     ui_plot.update()
 
 
 with ui.row():
-    ui.label('Transcations').style(
-        label_style)
+    ui.label('Transcations').style(label_style)
     ui.space()
     ui_code_selector = ui.select(
         all_stock_codes, value=all_stock_codes[0], on_change=update_plot)
@@ -190,6 +196,7 @@ fig = go.Figure()
 fig.update_layout(margin=dict(l=0, r=0, t=0, b=0))
 ui_plot = ui.plotly(fig).classes('w-full h-60')
 update_plot()
+
 
 @ui.refreshable
 def all_records_table_ui() -> None:
@@ -202,14 +209,16 @@ def all_records_table_ui() -> None:
     </q-td>
 ''')
 
+
 def update_2_tables_ui() -> None:
-    global df_etf, df_all_records_price, df_overview,total_profit
+    global df_etf, df_all_records_price, df_overview, total_profit
     df_etf = ak.fund_etf_spot_em()
     df_all_records_price = bind_price_to_all_records()
     df_overview = get_overview_table()
-    total_profit=get_total_profit(df_overview)
+    total_profit = get_total_profit(df_overview)
     overtiew_table_ui.refresh()
     all_records_table_ui.refresh()
+
 
 all_records_table_ui()
 ui.button('update', on_click=update_2_tables_ui)
